85cd059fd51f29d6822b783a411fcace1d5363c3
Peter Peck <karatekid4christ@gmail.com>
2017-10-19 16:01:23 -0500

    Adding the click function and grabbing the values from the inputs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
 
+	//Setting global variables
+	var TrainName = " ";
+	var Destination = " ";
+	var Frequency = " ";
+	var NextArrival = " ";
+	var MinutesAway = 0;
+
 	// Initialize Firebase
 	var config = {
 	    apiKey: "AIzaSyD3rIedSTLHdZqgZMnXE9rEPdhw2ES22hA",
@@ -12,18 +19,27 @@ $( document ).ready(function() {
 
 	firebase.initializeApp(config);
 
+	//This is how we access the data in firebase by setting it to a variable
 	var database = firebase.database();
 
+	//Going into the database and console logging ALL of the values in the database in firebase. The parameter is "snapshot"
 	database.ref().on("value", function(snapshot) {
 		console.log(snapshot.val())
-	}
+	})
 
 	database.ref().on("child_added", function(snapshot) {
 		console.log(snapshot.val())
-	}
+	})
 
 	database.ref().on("child_removed", function(snapshot) {
 		console.log(snapshot.val())
-	}
+	})
+
+
+	//Grabbing the values from the inputs and setting them to the global variables
+	$("#submit").on("click", function() {
+		TrainName = $("#trainname").val().trim();
+	})
+
 
 });
